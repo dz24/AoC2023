e686a309861863bc0739848a68804fedd90ee861
dz24 <daniel.t.zhang@ntnu.no>
2023-12-03 12:08:18 +0100

    complete

--- 03/a.py
+++ 03/a.py
@@ -1,10 +1,8 @@
-NSYM = '0123456789.'
+NSYM = "0123456789."
 NINE = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
 
 
-
-
-def parser(inp: str, pad: int=0) -> list[str]:
+def parser(inp: str, pad: int = 1) -> list[str]:
     # parses and pads grid, and finds symbol coordinates.
     grid = []
     sym_coord = []
@@ -17,98 +15,44 @@ def parser(inp: str, pad: int=0) -> list[str]:
                     sym_coord.append([idx_x + pad, idx_y + pad])
 
     if pad:
-        str_len = len(grid[0])+pad*2
-        grid2 = ['.'*str_len]
+        str_len = len(grid[0]) + pad * 2
+        grid2 = ["." * str_len]
         for line in grid:
-            grid2.append('.' + line + '.')
-        grid2.append('.'*str_len)
+            grid2.append("." + line + ".")
+        grid2.append("." * str_len)
         return grid2, sym_coord
     return grid, sym_coord
 
 
 def nums_x(line, y_idx, x_idx, numdic):
-    idxs = []
-    nums = []
-    num = []
-
+    num, idxs = [], []
     for i, let in enumerate(line):
         if let.isnumeric():
             num.append(let)
             idxs.append(i)
         else:
-            if any(abs(x_idx-j)<=1 for j in idxs):
-                numdic[str(idxs[0])+str(y_idx)] = int(''.join(num))
-            num = []
-            idxs = []
+            if any(abs(x_idx - j) <= 1 for j in idxs):
+                numdic[f"{idxs[0]}-{y_idx}"] = int("".join(num))
+            num, idxs = [], []
 
-def find_nums_sym(grid, coords):
+
+def part_ab(inp: str) -> int:
+    grid, coords = parser(inp)
     # find nums given grid and symbol coordinates.
-    nums = []
-    for i in grid:
-        print(i)
-    print(' ')
-    numdic = {}
+    summ, numdic = 0, {}
     for coord in coords:
+        numdic2 = {}
         idx, idy = coord[0], coord[1]
-        top, center, bot = [], [], []
+        nums_x(grid[idy - 1], idy - 1, idx, numdic2)
+        nums_x(grid[idy], idy, idx, numdic2)
+        nums_x(grid[idy + 1], idy + 1, idx, numdic2)
+        if len(numdic2) == 2:
+            values = list(numdic2.values())
+            summ += values[0] * values[1]
+        numdic.update(numdic2)
 
-        nums_x(grid[idy-1], idy-1, idx, numdic)
-        nums_x(grid[idy], idy, idx, numdic)
-        nums_x(grid[idy+1], idy+1, idx, numdic)
-
-        # if coord == '#':
-        #     exit('a')
-
-        # exit('ba')
-        # print('bear', grid[idy-1])
-        # print(idx, grid[idy-1].split('.'))
-        # exit('ba')
-
-        # for head in grid[idy-1][:idx-1][::-1]:
-        #     if top.isnumeric():
-        #         number.insert(0, head)
-        #     else:
-        #         break
-
-        # print(grid[idy-1][:idx-1][::-1])
-        # exit('ba')
+    return sum(list(numdic.values())), summ
 
 
-
-        # for dx, dy in NINE:
-        #     grid_el = grid[idy+dy][idx+dx]
-        #     if grid_el.isnumeric():
-        #         number = [grid_el]
-        #         for head in grid[idy+dy][:idx+dx][::-1]:
-        #             if head.isnumeric():
-        #                 number.insert(0, head)
-        #             else:
-        #                 break
-        #         for tail in grid[idy+dy][idx+dx+1:]:
-        #             if tail.isnumeric():
-        #                 number.append(tail)
-        #             else:
-        #                 break
-
-        #         print('plum', grid[idy+dy])
-        #         print('head', grid[idy+dy][:idx+dx][::-1])
-        #         print('tail', grid[idy+dy][idx+dx+1:])
-        #         print('numbs', number)
-        #         # exit('a')
-        #         # nums
-        # exit('snow')
-    print(numdic)
-    print(sum(list(numdic.values())))
-
-def part_a(inp: str, pad: int=0) -> int:
-    grid, coords = parser(inp, pad)
-    find_nums_sym(grid, coords)
-
-    print('shark')
-
-
-example1 = "./example1.txt"
-part_a_txt = './part_a.txt'
-# print(part_a(example1))
-part_a(example1, 1)
-part_a(part_a_txt, 1)
+part_a_txt = "./part_a.txt"
+print(part_ab(part_a_txt), (532445, 79842967))
